47652cae515e339c0ed7ccfcc6f8d765907b2551
AarSchoIg <aaron954@revature.net>
2022-03-11 15:43:24 -0500

    Initialized variables for player name and inventory as well as monster HP.
    Created variables for each characteristic of the player including: Health, defense, name, loot, weapon. created variables for location, and monster HP and general attack. Created a method for starting the game called gameStart: Displays message welcoming player to game and describes game

--- src/main/scala/AdventureGame/MainGame.scala
+++ src/main/scala/AdventureGame/MainGame.scala
@@ -1,7 +1,29 @@
 package AdventureGame
 
 object MainGame {
+  //Set up of taking user input and initial variables
+  scala.io.StdIn.readLine("")
+
+  protected var AdventurerHP: Int = 0
+  protected var Adventurerdefense: Int = 0
+  var Adventurerloot: String = _
+  var Adventurerweapon: String = _
+  var attack: Int = 0
+  var location: String = _
+  var monsterHP: Int = 0
+
+
+  //main game inital start simple version
   def main(args: Array[String]): Unit = {
-    
+
+    def gameStart() : Unit = {
+      println("Welcome to Grimm Tales! Press enter to start")
+      println("Grimm Tales is an exciting adventure game filled with monsters, exploration, combat, and treasure!" +
+        "press enter to continue.")
+      
+
+    }
+
+
   }
 }
